35869ef6e5d74b90d980890601584973a2b41867
Veronica Valeros <vero.valeros@gmail.com>
2024-02-15 17:36:47 +0100

    Add schema for the translation DB tables

---
+++ assets/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS translation_parameters (
+    translation_parameters_id   INTEGER PRIMARY KEY,
+    translation_tool_name       TEXT,
+    translation_tool_commit     TEXT,
+    translation_model           TEXT,
+    translation_config          TEXT
+);
+
+
+
+CREATE TABLE IF NOT EXISTS message_translation (
+    translation_id              INTEGER PRIMARY KEY,
+    translation_parameter_id    INTEGER,
+    message_id                  INTEGER,
+    translation_text            TEXT,
+    translation_timestamp       TIMESTAMPTZ(0),
+    FOREIGN KEY (translation_parameter_id) REFERENCES translation_parameters(translation_parameters_id),
+    FOREIGN KEY (message_id) REFERENCES messages(message_id)
+);
