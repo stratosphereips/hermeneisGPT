47bd604dd3722ea5ae4bda8dfdd54890c8a59d50
Veronica Valeros <vero.valeros@gmail.com>
2024-02-16 16:33:57 +0100

    Add get_current_commit() function

--- lib/utils.py
+++ lib/utils.py
@@ -2,3 +2,16 @@
 Various utilities associated with hermeneisGPT.
 """
 
+import subprocess
+
+
+def get_current_commit():
+    """
+    Function uses subprocess to retrieve the last commit of the tool.
+    """
+    try:
+        commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+        return commit_hash
+    except subprocess.CalledProcessError:
+        raise subprocess.CalledProcessError
+
