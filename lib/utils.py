c31dcb1a982a0b2d953b00e1d27e3f2fb90c777c
Veronica Valeros <vero.valeros@gmail.com>
2024-02-16 16:34:26 +0100

    Add function to calculate sha256 of file

--- lib/utils.py
+++ lib/utils.py
@@ -3,6 +3,7 @@ Various utilities associated with hermeneisGPT.
 """
 
 import subprocess
+import hashlib
 
 
 def get_current_commit():
@@ -14,4 +15,18 @@ def get_current_commit():
         return commit_hash
     except subprocess.CalledProcessError:
         raise subprocess.CalledProcessError
+
+
+def get_file_sha256(file_path):
+    """
+    Calculate the sha256 of a given file and return it.
+    """
+    sha256_hash = hashlib.sha256()
+    try:
+        with open(file_path, "rb") as f:
+            for byte_block in iter(lambda: f.read(4096), b""):
+                sha256_hash.update(byte_block)
+        return sha256_hash.hexdigest()
+    except FileNotFoundError:
+        raise FileNotFoundError
 
